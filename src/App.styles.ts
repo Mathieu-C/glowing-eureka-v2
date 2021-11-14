bc33477e609e9c18815eb950d930678973301191
Mathieu-C <chatillon.mathieu@gmail.com>
2021-11-14 21:44:29 +0100

    Modify App styles

---
+++ src/App.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 580px;
+  padding: 12px;
+  position: relative;
+`;
+
+const Styles = {
+  Wrapper,
+};
+
+export default Styles;
