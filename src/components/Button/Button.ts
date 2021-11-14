60692dde73194426668eb6cfd5a65d65fcae757e
Mathieu-C <chatillon.mathieu@gmail.com>
2021-11-14 21:41:50 +0100

    Add Button component

---
+++ src/components/Button/Button.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: white;
+  border-radius: 4px;
+  border: 1px solid #cccccc;
+  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.2);
+  color: #797874;
+  cursor: pointer;
+  font-family: "Helvetica Neue", sans-serif;
+  font-size: 17px;
+  font-weight: 700;
+  padding: 8px 24px;
+`;
+
+export default Button;
