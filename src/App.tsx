c1ba8aac183b7ed3d501df2ff7cfe93e5ce0a990
Mathieu-C <chatillon.mathieu@gmail.com>
2021-11-15 23:06:06 +0100

    Modify App index

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import S from "./App.styles";
+import NewReview from "./components/NewReview";
 import Review from "./components/Review";
 import Summary from "./components/Summary";
 import { Review as IReview } from "./shared/types";
@@ -15,11 +16,24 @@ function App() {
       .then((res) => setReviews(res.data));
   }, []);
 
+  useEffect(() => {
+    const source = new EventSource(
+      `${process.env.REACT_APP_API_ENDPOINT}/reviews/live`
+    );
+
+    source.addEventListener("created", function (event: any) {
+      setReviews((current) => [...current, JSON.parse(event.data)]);
+    });
+
+    return () => source.close();
+  }, []);
+
   return (
     <S.Wrapper>
       <h1>The Minimalist Entrepreneur</h1>
       <Summary openModal={() => setModal(true)} reviews={reviews} />
-      {modal && <div>modal</div>}
+      {modal && <NewReview close={() => setModal(false)} />}
+      <h2>Reviews</h2>
       {reviews.map((review) => (
         <Review key={review.id} review={review} />
       ))}
